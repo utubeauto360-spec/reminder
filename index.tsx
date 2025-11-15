008ec6834d03a597682845e4c2872e7fce92bab7
utubeauto360-spec <utubeauto360@gmail.com>
2025-11-15 11:00:19 +0500

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import OnboardingScreen from "./components/OnboardingScreen";
+
+export default function Index() {
+  // Placeholder for onboarding completion logic
+  const handleComplete = () => {
+    // TODO: Navigate to main app or set onboarding complete flag
+  };
+  return <OnboardingScreen onComplete={handleComplete} />;
+}
+
